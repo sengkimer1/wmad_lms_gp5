8e6f55e9debb5b0a4a88936f79fefa2385a66467
khonsreyvorleak5796-4 <sreyvorleak.khon@institute.pse.ngo>
2024-08-06 14:02:34 +0700

    book issue

--- src/pages/bookIssue/BookIssuePage.jsx
+++ src/pages/bookIssue/BookIssuePage.jsx
@@ -1,9 +1,69 @@
-const BookIssuePage = () => {
+import { useState, useEffect } from "react";
+
+const URL = `http://localhost:3000/api/book_issues`;
+const token = localStorage.getItem("token");
+
+
+function BookIssueTable() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(URL, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const result = await response.json();
+      setBooks(result);
+    };
+    fetchData();
+  }, []);
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold">Book Issue</h1>
+    <div className="container mx-auto">
+      <h1 className="text-3xl mb-4">Book Issue</h1>
+      
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 mb-4">
+        Create
+      </button>
+      <div className="overflow-hidden rounded-xl border border-gray-500">
+        <table className="min-w-full bg-white">
+          <thead>
+            <tr>
+              <th className="px-6 py-3 border-b border-black text-left">Action</th>
+              <th className="px-6 py-3 border-b border-black text-left">ISBN</th>
+              <th className="px-6 py-3 border-b border-black text-left">Title</th>
+              <th className="px-6 py-3 border-b border-black text-left">Member</th>
+              <th className="px-6 py-3 border-b border-black text-left">Librarian</th>
+              <th className="px-6 py-3 border-b border-black text-left">Issue Date</th>
+              <th className="px-6 py-3 border-b border-black text-left">Due Date</th>
+              <th className="px-6 py-3 border-b border-black text-left">Return Date</th>
+              <th className="px-6 py-3 border-b border-black text-left">State</th>
+            </tr>
+          </thead>
+          <tbody>
+            {books.map((book, i) => (
+              <tr key={i} className="even:bg-gray-100">
+                <td className="px-6 py-4 border-b border-black">
+                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+                    View
+                  </button>
+                </td>
+                <td className="px-6 py-3 border-b border-black">{book.book.isbn}</td>
+                <td className="px-6 py-3 border-b border-black">{book.book.title}</td>
+                <td className="px-6 py-3 border-b border-black">{book.member.fullname}</td>
+                <td className="px-6 py-3 border-b border-black">{book.processed_by.username}</td>
+                <td className="px-6 py-3 border-b border-black">{book.issue_date}</td>
+                <td className="px-6 py-3 border-b border-black">{book.due_date}</td>
+                <td className="px-6 py-3 border-b border-black">{book.return_date}</td>
+                <td className="px-6 py-3 border-b border-black">{book.status_id}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
-};
-
-export default BookIssuePage;
+  
+}
+export default BookIssueTable;
